4c00535ad8195b3c0e8af4d796e1a53383668804
VitorHWitt <45514663+VitorHWitt@users.noreply.github.com>
2022-03-25 12:15:48 -0300

    Desafio 3

---
+++ src/Desafio_3_Dividindo_X_por_Y.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Escrever um algoritmo que leia 2 números e imprima o resultado da divisão do primeiro pelo segundo.
+ * Caso não for possível, mostre a mensagem “divisao impossivel” para os valores em questão.
+ */
+
+public class Desafio_3_Dividindo_X_por_Y {
+
+    public static void main(String[] args) throws IOException {
+        //complete os espaços com sua solução
+        Scanner leitor = new Scanner(System.in);
+        int N = leitor.nextInt();
+        int X, Y;
+        for (int i = 0; i < N; i++) {
+            X = leitor.nextInt();
+            Y = leitor.nextInt();
+            if (Y == 0.0) System.out.println("divisao impossivel");
+            else System.out.printf("%.1f %n", (double) X / Y);
+        }
+    }
+}
