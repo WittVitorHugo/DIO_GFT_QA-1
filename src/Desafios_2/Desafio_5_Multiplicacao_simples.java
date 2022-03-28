8c6d9a76db4c00a55d8c5195729a0ea951475613
VitorHWitt <45514663+VitorHWitt@users.noreply.github.com>
2022-03-28 14:43:28 -0300

    Desafios 2 - Exercicio 5

---
+++ src/Desafios_2/Desafio_5_Multiplicacao_simples.java
@@ -0,0 +1,27 @@
+package Desafios_2;
+
+import java.util.Scanner;
+
+/**
+ * Você receberá dois valores inteiros. Faça a leitura e em seguida calcule o produto entre estes dois valores.
+ * Atribua esta operação à variável PROD, mostrando esta de acordo com a mensagem de saída "PROD = "
+ */
+
+public class Desafio_5_Multiplicacao_simples {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A, B, PROD;
+
+        A = sc.nextInt();
+        B = sc.nextInt();
+
+        PROD = A * B;
+
+        System.out.println("PROD = " + PROD);
+
+        sc.close();
+
+    }
+}
